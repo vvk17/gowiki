4ac047809006363e35dc35ddde57ace89f359335
vvk17 <v12345@mail.ru>
2020-05-22 16:09:28 +0300

    Update wiki.go

--- wiki.go
+++ wiki.go
@@ -98,7 +98,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	p1 := &Page{Title: "Test Page", Body: []byte("This is a test page - тестовая страница")}
 	p1.save()
-	p2, _ := loadPage("Test Page")
+	_ , _ := loadPage("Test Page")
 //	fmt.Println(p2.Title, string(p2.Body))
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
